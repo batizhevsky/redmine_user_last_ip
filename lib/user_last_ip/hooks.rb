69acb8c36051e18340856b99372d0931cd48f18a
Leonid Batizhevsky <the.leonko@gmail.com>
2012-08-14 13:30:48 +0400

    add extra logging to console

--- lib/user_last_ip/hooks.rb
+++ lib/user_last_ip/hooks.rb
@@ -2,8 +2,10 @@ module UserLastIp
   class Hooks < Redmine::Hook::ViewListener
     render_on :view_account_left_bottom, :partial => 'user/last_ip'
     def controller_account_success_authentication_after(context={})
-      context[:user].write_attribute(:last_ip,context[:request].remote_ip)
-      context[:user].save
+      c_user = context[:user]
+      c_user.write_attribute(:last_ip,context[:request].remote_ip)
+      c_user.save
+      Logger.new(STDOUT).info("\n#{c_user}(#{c_user.mail}) was logined from #{context[:request].remote_ip} \n")
     end
   end
 end
